a2967e72a2247e12eb01d2542c8e4f725dfa3191
Mateus Dizeu <122738432+DizeuM@users.noreply.github.com>
2026-09-08 10:35:25 -0300

    chore: add initial structure

---
+++ code/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Projeto Mentoria FICR 2026.2",
+  description: "Sistema de Gestão de documentos",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="PT-BR"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ code/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="align-center flex h-full flex-col items-center justify-between ">
+      <h1>HOME</h1>
+    </div>
+  );
+}
